2fdc82c130c08f04b45e4981c7bb0687be5d29ca
mona <mona0955@stud.kea.dk>
2021-10-12 23:42:51 +0200

    first commit

--- src/main/java/com/example/demo/repository/ActivityRepository.java
+++ src/main/java/com/example/demo/repository/ActivityRepository.java
@@ -4,4 +4,5 @@ import com.example.demo.model.Activity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ActivityRepository extends JpaRepository<Activity, Integer> {
+
 }
