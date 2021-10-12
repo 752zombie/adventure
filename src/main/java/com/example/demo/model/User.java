877de6543c54cbe029d4918d5230d47ea27a4514
Matthias <mdjer@hotmail.com>
2021-10-12 12:23:27 +0200

    first commmit Matthias

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,21 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    Integer id;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+}
