4f72f1fb4feeb2b14427900f75a0ef732ca7ad83
mona <mona0955@stud.kea.dk>
2021-10-15 15:24:08 +0200

    frontpage added

--- src/main/java/com/example/demo/controller/NavigationController.java
+++ src/main/java/com/example/demo/controller/NavigationController.java
@@ -1,12 +1,45 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class NavigationController {
+
     @GetMapping("/login")
-    public String getUser(){
+    public String get_user(){
         return "login";
     }
+
+    @GetMapping("/create_reservation")
+    public String create_reservation(){
+        return "create_reservation";
+    }
+
+    @GetMapping("/delete_reservation")
+    public String delete_reservation(){
+        return "delete_reservation";
+    }
+
+    @GetMapping("/reservation_list")
+    public String show_reservation_list(){
+        return "reservation_list";
+    }
+
+    @GetMapping("/create_activity")
+    public String create_activity(){
+        return "create_activity";
+    }
+
+    @GetMapping("/delete_activity")
+    public String delete_activity(){
+        return "delete_activity";
+    }
+
+    @GetMapping("/activity_list")
+    public String show_activity_list(){
+        return "activity_list";
+    }
 }
+
