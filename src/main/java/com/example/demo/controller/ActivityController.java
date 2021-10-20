caab858d10ebca09f668ad15fde1fcd89cbcf1f5
WafaaAjan <ou_ajan@hotmail.com>
2021-10-20 15:21:17 +0200

    create activity oprettet

--- src/main/java/com/example/demo/controller/ActivityController.java
+++ src/main/java/com/example/demo/controller/ActivityController.java
@@ -1,8 +1,14 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Activity;
+import com.example.demo.repository.ActivityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class ActivityController {
@@ -16,4 +22,19 @@ public class ActivityController {
     public String testPage() {
         return "test";
     }
+
+/*
+    @Autowired
+    ActivityRepository activityRepository;
+
+    @GetMapping("/activities")
+    public String createActivity() {
+        Activity activity = new Activity();
+        activityRepository.save(activity);
+
+        List<Activity> activities = activityRepository.findAll();
+
+        return "activities";
+    }*/
 }
+
